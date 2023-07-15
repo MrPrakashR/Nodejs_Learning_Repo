434eec1bb7d14fd58753dff4e819d0ae5dc3403c
MrPrakashR <77004328+MrPrakashR@users.noreply.github.com>
2023-07-15 12:00:31 +0000

    All Middle Ware Handling

--- index.js
+++ index.js
@@ -5,11 +5,20 @@
 // Error-handling middleware
 
 const express = require("express")
+const path = require("path")
 const routes = express.Router()
+const logger = require("morgan")
+const multer = require("multer")
+const upload = multer({dest:"./public/upload"})
 
 const app = express()
 
 const PORT = 5001
+
+//In Build Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use("/static",express.static(path.join(__dirname,"public")))
 
 //Application level middleware
 const loggerMiddleWare = (req,resp,next) => {
@@ -19,11 +28,14 @@ const loggerMiddleWare = (req,resp,next) => {
 
 app.use(loggerMiddleWare)
 
+//Third Party Middleward
+app.use(logger("combined"))
+
 //Router Level MiddleWare
 app.use("/api/users",routes)
 
 const fakeAuth = (req,res,next) => {
-    const authStatus = false
+    const authStatus = true
     if (authStatus) {
         console.log("User AuthStatus:",authStatus)
         next()
@@ -38,12 +50,49 @@ const getUsers = (req,res) => {
 }
 
 const createUser = (req,res) => {
+    console.log("This is Request we got from user ",req.body)
     res.json({message:"Create New Users"})
 }
 
 routes.use(fakeAuth)
 routes.route("/").get(getUsers).post(createUser)
 
+//Error Handler MiddleWare
+const errorHandler = (err,req,res,next) => {
+    const statusCode = res.statusCode ? res.statusCode : 500
+    res.status(statusCode)
+    switch (statusCode) {
+        case 401:
+            res.json({title:"UnAuthorized",message:err.message})
+            break
+        case 404:
+            res.json({title:"Not Found",message:err.message})
+            break
+        case 500:
+            res.json({title:"Server Error",message:err.message})
+            break
+        default:
+            break
+    }
+}
+
+//Upload Api
+app.post("/upload",upload.single("image"),(req,res,next)=>{
+    console.log(req.file,req.body)
+},(err,req,res,next)=>{
+    res.status(400).send({err:err.message})
+})
+
+app.all("*",(req,res)=>{
+    res.status(404)
+
+    throw Error("Route Not Found")
+})
+
+app.use(errorHandler)
+
+
 app.listen(PORT, () => {
     console.log(`App Running on port ${PORT}`)
 })
+
