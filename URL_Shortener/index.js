ec2c1e0d0004540a782a1320a230c6098ae2a380
MrPrakashR <77004328+MrPrakashR@users.noreply.github.com>
2023-08-05 19:45:56 +0000

    EJS Server Side Rendering

--- URL_Shortener/index.js
+++ URL_Shortener/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const urlRoute = require("./routes/url")
 const URL = require("./models/url")
+const staticRoute = require("./routes/staticRouter")
+const path = require("path")
 const {connectToMongooseDB} = require('./connect')
 
 const app = express()
@@ -10,10 +12,24 @@ connectToMongooseDB('mongodb+srv://admin:admin@ecommerce-prakash.bybdb3e.mongodb
     console.log("Mongodb connected")
 )
 
+app.set("view engine","ejs")
+app.set("views",path.resolve("./views"))
+
 app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.use("/url",urlRoute)
 
-app.get("/:shortId", async (req,res)=> {
+app.get("/test", async (req,res) => {
+    const allUrls = await URL.find({})
+    res.render("Home",{
+        urls:allUrls
+    })
+})
+
+app.use("/",staticRoute)
+
+app.get("/url/:shortId", async (req,res)=> {
     const shortId = req.params.shortId
     const entry = await URL.findOneAndUpdate({
         shortId
