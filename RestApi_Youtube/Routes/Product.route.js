026c67eaec34ea9e9e4d4834e3d4aa1a7e9bccf0
MrPrakashR <77004328+MrPrakashR@users.noreply.github.com>
2023-09-10 15:20:11 +0000

    Saving Product to mongodb

--- RestApi_Youtube/Routes/Product.route.js
+++ RestApi_Youtube/Routes/Product.route.js
@@ -1,12 +1,32 @@
 const express = require("express")
 const router = express.Router()
 
+const Product = require("../Models/Product.model")
+
 router.get("/",(req,res,next)=>{
     res.send("getting a list of all products....")
 })
 
-router.post("/",(req,res,next)=>{
-    res.send("product created")
+router.post("/",async (req,res,next)=>{
+ 
+    try {
+        const product = Product(req.body)
+        const result = await product.save()
+        res.send(result)
+    } catch (error) {
+        console.log(error.message)
+    }
+
+    // const product = Product({
+    //     name:req.body.name,
+    //     price:req.body.price
+    // })
+    // product.save().then(result => {
+    //     console.log(result)
+    //     res.send(result)
+    // }).catch(err=>{
+    //     console.log(err.message)
+    // })
 })
 
 router.get("/:id",(req,res,next)=>{
