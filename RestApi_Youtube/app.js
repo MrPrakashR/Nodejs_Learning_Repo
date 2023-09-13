43e9f1dad09e06400ee11d4e2ebb4ce5dc54ea2c
MrPrakashR <77004328+MrPrakashR@users.noreply.github.com>
2023-09-13 17:36:38 +0000

    docker setup

--- RestApi_Youtube/app.js
+++ RestApi_Youtube/app.js
@@ -47,7 +47,7 @@ app.use((err,req,res,next)=>{
     })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log(`Server started on port ${PORT}....`)
 })
